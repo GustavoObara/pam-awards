85c8894a86b1a3d955a89e4d49c86404a3242a3d
GustavoObara <luizobara@gmail.com>
2024-12-18 07:23:29 -0300

    feat: add new /spoiler route to show results ahead of time

---
+++ app/spoiler/page.tsx
@@ -0,0 +1,22 @@
+import NavigationButton from "@/components/NavigationButton";
+import WinnersComponent from "@/components/WinnersComponent";
+
+export default function SpoilerPage() {
+
+    return (
+        <div className="w-full min-h-screen p-6">
+            <div className="flex justify-between space-x-8 mb-3">
+                <NavigationButton 
+                    title="Voltar"
+                    path="/"
+                />
+                <NavigationButton 
+                    title="Formulário"
+                    path="/form"
+                />
+            </div>
+
+            <WinnersComponent/>
+        </div>
+    );
+}
