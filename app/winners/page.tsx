e508b9be1b786f5c128ea18df19706eacb9ad680
GustavoObara <luizobara@gmail.com>
2024-12-20 13:57:14 -0300

    fix: /winners

--- app/winners/page.tsx
+++ app/winners/page.tsx
@@ -2,10 +2,6 @@ import NavigationButton from "@/components/NavigationButton";
 import WinnersComponent from "@/components/WinnersComponent";
 
 export default function WinnersPage() {
-    const targetDate = new Date("2024-12-20T12:00:00-03:00").getTime();
-    const currentDate = new Date().getTime();
-
-    const isEventDate = currentDate >= targetDate;
 
     return (
         <div className="w-full min-h-screen p-6">
@@ -20,19 +16,7 @@ export default function WinnersPage() {
                 />
             </div>
 
-            {isEventDate ? (
-                <WinnersComponent />
-            ) : (
-                <div className="text-center text-xl font-bold uppercase lg:text-3xl text-white ">
-                    O evento ainda não começou.
-                    <div className="mt-3">
-                        <NavigationButton 
-                            title="Voltar ao Timer"
-                            path="/timer"
-                        />
-                    </div>
-                </div>
-            )}
+            <WinnersComponent />
         </div>
     );
 }
